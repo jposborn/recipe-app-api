dd9d5154a877adb63121fcfa4539a6459e0015ef
John Osborn <jposborn@outlook.com>
2022-09-16 18:10:41 +0100

    Test Added

--- app/app/test.py
+++ app/app/test.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
@@ -13,3 +14,4 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+        
