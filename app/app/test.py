0063ab9af413b6a4249cde9724e543d0e6ec3599
John Osborn <jposborn@outlook.com>
2022-09-19 14:19:27 +0100

    Add Postgresql Config

--- app/app/test.py
+++ app/app/test.py
@@ -6,7 +6,6 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-
 class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
@@ -14,3 +13,10 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
+      
