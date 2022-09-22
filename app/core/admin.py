2b63d8e67b0f221ce07a1421b15782cd190ea810
John Osborn <jposborn@outlook.com>
2022-09-22 10:14:21 +0100

    Fix Linting

--- app/core/admin.py
+++ app/core/admin.py
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
         (
             _('Permissions'),
             {
-                'fields':(
+                'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
@@ -30,7 +30,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields':(
+            'fields': (
                 'email',
                 'password1',
                 'password2',
@@ -43,8 +43,4 @@ class UserAdmin(BaseUserAdmin):
     )
 
 
-
-
-
 admin.site.register(models.User, UserAdmin)
-
